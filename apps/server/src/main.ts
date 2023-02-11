6538a89ede9ce990d834b0e887137c49ba1d3e16
Timothée Vialatoux <ext-timothee@photoroom.com>
2023-02-11 17:55:58 +0100

    Add shared library

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,15 +3,18 @@
  * This is only a minimal backend to get started.
  */
 
+import { schema } from '@oms-monorepo/shared';
 import express from 'express';
 import * as path from 'path';
+import { z } from "zod";
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to server!' });
+  const result: z.infer<typeof schema> = { message: 'Welcome to server!' };
+  res.send(result);
 });
 
 const port = process.env.PORT || 3333;
